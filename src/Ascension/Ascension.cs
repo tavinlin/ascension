6c71ba94baa79924ce0b8fa12d7fff9b0d4fbba9
Tavin <tavinlin1519@gmail.com>
2021-04-26 11:33:38 -0400

    Implemented enemy's attack pattern Further refractored the code to improve readability

--- src/Ascension/Ascension.cs
+++ src/Ascension/Ascension.cs
@@ -31,101 +31,17 @@ namespace Ascension
                     // Number prompt
                     int intInput = User.Int16Input("Choose a spot from 1 - 10: ") - 1;
                     var spot = area[intInput];
-                    if(spot.isOccupied)
+                    if (spot.isOccupied == true && spot.isExplored == false)
                     {
                         Console.Clear();
                         Console.WriteLine($"You encounter {spot.Enemy.Name}");
                         spot.isExplored = true;
-                        bool isBattle = true;
-                        int damageDealt = 0;
-                        Random rnd = new Random();
                         // start battle
-                        while(isBattle)
-                        {
-                            Console.WriteLine($"{player.Name}: {player.CurrentHealth} - {spot.Enemy.Name}: {spot.Enemy.CurrentHealth}");
-                            var atkInput = User.Int16Input("Choose an attack: 1. vertical, 2. horizontal, 3. thrust: ");
-                            var playerOptions = new Dictionary<int, string>(){
-                                {1, "vertical"},
-                                {2, "horizontal"},
-                                {3, "thrust"}
-                            };
-                            // TODO: Enemy will loop through an array and start over the iteration in order simulate attack pattern
-                            string[] monsterOptions = new string[] {"vertical", "horizontal", "thrust"};
-                            switch(monsterOptions[rnd.Next(1,4) - 1])
-                            {
-                                case "vertical":
-                                    Console.WriteLine("Enemy chose vertical attack!");
-                                    if(playerOptions[atkInput] == "vertical")
-                                    {
-                                        Console.WriteLine($"{player.Name} and {spot.Enemy.Name} cancel each other attack.");
-                                    }
-                                    else if(playerOptions[atkInput] == "horizontal")
-                                    {
-                                        damageDealt = spot.Enemy.AtkPoint;
-                                        Console.WriteLine($"{player.Name} took {damageDealt} damage!");
-                                        player.CurrentHealth -= damageDealt;
-                                    }
-                                    else
-                                    {
-                                        damageDealt = player.AtkPoint;
-                                        Console.WriteLine($"{spot.Enemy.Name} took {damageDealt} damage!");
-                                        spot.Enemy.CurrentHealth -= damageDealt;
-                                    }
-                                    break;
-                                case "horizontal":
-                                    Console.WriteLine("Enemy chose horizontal attack!");
-                                    if(playerOptions[atkInput] == "horizontal")
-                                    {
-                                        Console.WriteLine($"{player.Name} and {spot.Enemy.Name} cancel each other attack.");
-                                    }
-                                    else if(playerOptions[atkInput] == "thrust")
-                                    {
-                                        damageDealt = spot.Enemy.AtkPoint;
-                                        Console.WriteLine($"{player.Name} took {damageDealt} damage!");
-                                        player.CurrentHealth -= damageDealt;
-                                    }
-                                    else
-                                    {
-                                        damageDealt = player.AtkPoint;
-                                        Console.WriteLine($"{spot.Enemy.Name} took {damageDealt} damage!");
-                                        spot.Enemy.CurrentHealth -= damageDealt;
-                                    }
-                                    break;
-                                case "thrust":
-                                    Console.WriteLine("Enemy chose thrust attack");
-                                    if(playerOptions[atkInput] == "thrust")
-                                    {
-                                        Console.WriteLine($"{player.Name} and {spot.Enemy.Name} cancel each other attack.");
-                                    }
-                                    else if(playerOptions[atkInput] == "vertical")
-                                    {
-                                        damageDealt = spot.Enemy.AtkPoint;
-                                        Console.WriteLine($"{player.Name} took {damageDealt} damage!");
-                                        player.CurrentHealth -= damageDealt;
-                                    }
-                                    else
-                                    {
-                                        damageDealt = player.AtkPoint;
-                                        Console.WriteLine($"{spot.Enemy.Name} took {damageDealt} damage!");
-                                        spot.Enemy.CurrentHealth -= damageDealt;
-                                    }
-                                    break;
-                            }
-                            if(spot.Enemy.CurrentHealth <= 0)
-                            {
-                                Console.WriteLine($"{spot.Enemy.Name} Died!");
-                                Enemy.InstanceCounter -= 1;
-                                isBattle = false;
-                                spot.isOccupied = false;
-                            }
-                            else if(player.CurrentHealth <= 0)
-                            {
-                                Console.WriteLine($"{player.Name} Died!");
-                                isBattle = false;
-                                player.isDead = true;
-                                isPlaying = false;
-                            }
-                        }
+                        this.Battle(spot);
+                    }
+                    else if (spot.isExplored)
+                    {
+                        Console.WriteLine("You already explored this spot...");
                     }
                     else
                     {
@@ -141,19 +57,111 @@ namespace Ascension
         {
             Enemy[] enemies = new Enemy[]
             {
-                new Enemy("Satan", 14, 3, 0),
-                new Enemy("Kappa", 12, 1, 0),
-                new Enemy("Zombie", 4, 1, 0),
-                new Enemy("Banshee", 6, 2, 0),
-                new Enemy("Anima", 20, 4, 0),
-                new Enemy("Demon", 4, 2, 0)
+                new Enemy("Satan", 14, 3, 0, 5),
+                new Enemy("Kappa", 12, 1, 0, 5),
+                new Enemy("Zombie", 4, 1, 0, 5),
+                new Enemy("Banshee", 6, 2, 0, 5),
+                new Enemy("Anima", 18, 4, 0, 5),
+                new Enemy("Demon", 7, 2, 0, 5)
             };
             return enemies;
         }
 
-        private void Battle(Player player, Space spot)
+        private void Battle(Space spot)
         {
+            bool isBattle = true;
+            int damageDealt = 0;
+            int atkCount = 0;
+            var playerOptions = new Dictionary<int, string>() { { 1, "vertical" }, { 2, "horizontal" }, { 3, "thrust" } };
+            string[] generatedPattern = spot.Enemy.GenerateAtkPattern();
 
+            while (isBattle)
+            {
+                Console.WriteLine($"{player.Name}: {player.CurrentHealth} - {spot.Enemy.Name}: {spot.Enemy.CurrentHealth}");
+                var atkInput = User.Int16Input("Choose an attack: 1. vertical, 2. horizontal, 3. thrust: ");
+                // Enemy will loop through a string array of options and start over the iteration in order simulate attack pattern
+
+                switch (generatedPattern[atkCount])
+                {
+                    case "vertical":
+                        Console.WriteLine("Enemy chose vertical attack!");
+                        if (playerOptions[atkInput] == "vertical")
+                        {
+                            Console.WriteLine($"{player.Name} and {spot.Enemy.Name} cancel each other attack.");
+                        }
+                        else if (playerOptions[atkInput] == "horizontal")
+                        {
+                            damageDealt = spot.Enemy.AtkPoint;
+                            Console.WriteLine($"{player.Name} took {damageDealt} damage!");
+                            player.CurrentHealth -= damageDealt;
+                        }
+                        else
+                        {
+                            damageDealt = player.AtkPoint;
+                            Console.WriteLine($"{spot.Enemy.Name} took {damageDealt} damage!");
+                            spot.Enemy.CurrentHealth -= damageDealt;
+                        }
+                        break;
+                    case "horizontal":
+                        Console.WriteLine("Enemy chose horizontal attack!");
+                        if (playerOptions[atkInput] == "horizontal")
+                        {
+                            Console.WriteLine($"{player.Name} and {spot.Enemy.Name} cancel each other attack.");
+                        }
+                        else if (playerOptions[atkInput] == "thrust")
+                        {
+                            damageDealt = spot.Enemy.AtkPoint;
+                            Console.WriteLine($"{player.Name} took {damageDealt} damage!");
+                            player.CurrentHealth -= damageDealt;
+                        }
+                        else
+                        {
+                            damageDealt = player.AtkPoint;
+                            Console.WriteLine($"{spot.Enemy.Name} took {damageDealt} damage!");
+                            spot.Enemy.CurrentHealth -= damageDealt;
+                        }
+                        break;
+                    case "thrust":
+                        Console.WriteLine("Enemy chose thrust attack");
+                        if (playerOptions[atkInput] == "thrust")
+                        {
+                            Console.WriteLine($"{player.Name} and {spot.Enemy.Name} cancel each other attack.");
+                        }
+                        else if (playerOptions[atkInput] == "vertical")
+                        {
+                            damageDealt = spot.Enemy.AtkPoint;
+                            Console.WriteLine($"{player.Name} took {damageDealt} damage!");
+                            player.CurrentHealth -= damageDealt;
+                        }
+                        else
+                        {
+                            damageDealt = player.AtkPoint;
+                            Console.WriteLine($"{spot.Enemy.Name} took {damageDealt} damage!");
+                            spot.Enemy.CurrentHealth -= damageDealt;
+                        }
+                        break;
+                }
+                if (spot.Enemy.CurrentHealth <= 0)
+                {
+                    Console.WriteLine($"{spot.Enemy.Name} Died!");
+                    Enemy.InstanceCounter -= 1;
+                    isBattle = false;
+                    spot.isOccupied = false;
+                }
+                else if (player.CurrentHealth <= 0)
+                {
+                    Console.WriteLine($"{player.Name} Died!");
+                    isBattle = false;
+                    player.isDead = true;
+                    isPlaying = false;
+                }
+
+                atkCount += 1;
+                if (atkCount == generatedPattern.Length)
+                    atkCount = 0;
+            }
         }
+
+
     }
 }
--- src/Ascension/Enemy.cs
+++ src/Ascension/Enemy.cs
@@ -5,9 +5,8 @@ namespace Ascension
     class Enemy : Entity
     {
         public static int InstanceCounter {get; set;}
-        public string[] atkPattern;
-        private int AtkSize { get; set; }
-        public Enemy(string name, int maxHealth, int atk, int amr)
+        private int AtkPatternSize { get; set; }
+        public Enemy(string name, int maxHealth, int atk, int amr, int patternSize)
         {
             Name = name;
             MaxHealth = maxHealth;
@@ -15,6 +14,21 @@ namespace Ascension
             AtkPoint = atk;
             AmrPoint = amr;
             InstanceCounter += 1;
+            AtkPatternSize = patternSize;
+        }
+
+        public string[] GenerateAtkPattern()
+        {
+            string[] atkPattern = new string[AtkPatternSize];
+            string[] options = new string[] { "vertical", "horizontal", "thrust" };
+            Random rnd = new Random();
+
+            for(int i = 0; i < atkPattern.Length; i++)
+            {
+                atkPattern[i] = options[rnd.Next(1, 4) - 1];
+            }
+
+            return atkPattern;
         }
     }
 }
